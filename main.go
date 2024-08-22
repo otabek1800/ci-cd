30e00d31c9eb71d53c5edacbe756f9a9a1e0c487
otabek1800 <oxazratqulov@gmail.com>
2024-08-22 15:57:52 +0500

    sd

--- main.go
+++ main.go
@@ -6,11 +6,67 @@ import (
 
 func main() {
 	msg := seyHello("go")
+
+	msg = seyGoodbye("go")
+
+	msg = seyThankYou("go")
+
+	msg = seyWelcome("go")
+
+	msg = seyGoodMorning("go")
+
+	msg = seyGoodNight("go")
+
+	msg = seyGoodLuck("go")
+
+	msg = seyCongratulations("go")
+
+	msg = seySorry("go")
+
+	msg = seySeeYou("go")
+	
 	
 	println(msg)
+	
 
 }
 
 func seyHello(name string) string {
 	return fmt.Sprintf("Hello %s", name)
+}
+
+func seyGoodbye(name string) string {
+	return fmt.Sprintf("Goodbye %s", name)
+}
+
+func seyThankYou(name string) string {
+	return fmt.Sprintf("Thank you %s", name)
+}
+
+func seyWelcome(name string) string {
+	return fmt.Sprintf("Welcome %s", name)
+}
+
+func seyGoodMorning(name string) string {
+	return fmt.Sprintf("Good morning %s", name)
+}
+
+func seyGoodNight(name string) string {
+	return fmt.Sprintf("Good night %s", name)
+}
+
+func seyGoodLuck(name string) string {
+	return fmt.Sprintf("Good luck %s", name)
+}
+
+func seyCongratulations(name string) string {
+	return fmt.Sprintf("Congratulations %s", name)
+}
+
+func seySorry(name string) string {
+	return fmt.Sprintf("Sorry %s", name)
+}
+
+func seySeeYou(name string) string {
+	return fmt.Sprintf("See you %s", name)
 }
--- main_test.go
+++ main_test.go
@@ -12,3 +12,84 @@ func TestSeyHello(t *testing.T){
 	}
 
 }
+
+func TestSeyGoodbye(t *testing.T) {
+	msg := seyGoodbye("go")
+	want := "Goodbye go"
+
+	if msg != want {
+		t.Errorf("got %q, want %q", msg, want)
+	}
+}
+
+func TestSeyThankYou(t *testing.T) {
+	msg := seyThankYou("go")
+	want := "Thank you go"
+
+	if msg != want {
+		t.Errorf("got %q, want %q", msg, want)
+	}
+}
+
+func TestSeyWelcome(t *testing.T) {
+	msg := seyWelcome("go")
+	want := "Welcome go"
+
+	if msg != want {
+		t.Errorf("got %q, want %q", msg, want)
+	}
+}
+
+func TestSeyGoodMorning(t *testing.T) {
+	msg := seyGoodMorning("go")
+	want := "Good morning go"
+
+	if msg != want {
+		t.Errorf("got %q, want %q", msg, want)
+	}
+}
+
+func TestSeyGoodNight(t *testing.T) {
+	msg := seyGoodNight("go")
+	want := "Good night go"
+
+	if msg != want {
+		t.Errorf("got %q, want %q", msg, want)
+	}
+}
+
+func TestSeyGoodLuck(t *testing.T) {
+	msg := seyGoodLuck("go")
+	want := "Good luck go"
+
+	if msg != want {
+		t.Errorf("got %q, want %q", msg, want)
+	}
+}
+
+func TestSeyCongratulations(t *testing.T) {
+	msg := seyCongratulations("go")
+	want := "Congratulations go"
+
+	if msg != want {
+		t.Errorf("got %q, want %q", msg, want)
+	}
+}
+
+func TestSeySorry(t *testing.T) {
+	msg := seySorry("go")
+	want := "Sorry go"
+
+	if msg != want {
+		t.Errorf("got %q, want %q", msg, want)
+	}
+}
+
+func TestSeySeeYou(t *testing.T) {
+	msg := seySeeYou("go")
+	want := "See you go"
+
+	if msg != want {
+		t.Errorf("got %q, want %q", msg, want)
+	}
+}
